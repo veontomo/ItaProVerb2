c1774a720e69eb8453f3d3ccdf43a50371e6be6a
Mario Rossi <veontomo@gmail.com>
2015-09-09 22:00:29 +0200

    feat: create proverb adapter

---
+++ app/src/main/java/com/veontomo/itaproverb/api/ProverbAdapter.java
@@ -0,0 +1,85 @@
+package com.veontomo.itaproverb.api;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.veontomo.itaproverb.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mario Rossi on 09/09/2015 at 21:28.
+ *
+ * @author veontomo@gmail.com
+ * @since 0
+ */
+public class ProverbAdapter extends BaseAdapter {
+
+    /**
+     * Current context
+     */
+    private final Context mContext;
+    /**
+     * List of proverbs
+     */
+    private List<Proverb> mItems;
+
+    @Override
+    public int getCount() {
+        if (this.mItems == null) {
+            return 0;
+        }
+        return this.mItems.size();
+    }
+
+    public ProverbAdapter(Context context, List<Proverb> proverbs) {
+        this.mContext = context;
+        this.mItems = proverbs;
+        Log.i(Config.APP_NAME, "proverb adapter");
+    }
+
+    @Override
+    public Proverb getItem(int position) {
+        return this.mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.i(Config.APP_NAME, "proverb adapter: get view");
+        View row = convertView;
+
+        if (row == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.proverb_row, parent, false);
+            Holder holder = new Holder();
+            holder.text = (TextView) row.findViewById(R.id.layout_proverb_row_text);
+            row.setTag(holder);
+        }
+        Holder holder = (Holder) row.getTag();
+        holder.text.setText(this.getItem(position).text);
+        return row;
+    }
+
+    /**
+     * Loads proverbs.
+     * @param proverbs
+     */
+    public void load(List<Proverb> proverbs) {
+        this.mItems = proverbs;
+    }
+
+    private static class Holder {
+        public TextView text;
+    }
+}
--- app/src/main/java/com/veontomo/itaproverb/fragments/FragShowMulti.java
+++ app/src/main/java/com/veontomo/itaproverb/fragments/FragShowMulti.java
@@ -6,10 +6,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.veontomo.itaproverb.R;
 import com.veontomo.itaproverb.api.Config;
 import com.veontomo.itaproverb.api.Proverb;
+import com.veontomo.itaproverb.api.ProverbAdapter;
 
 import java.util.List;
 
@@ -17,7 +19,20 @@ import java.util.List;
  * A fragment to display many proverbs
  */
 public class FragShowMulti extends Fragment {
+    /**
+     * list of proverbs that this fragment should display
+     */
     private List<Proverb> mProverbs;
+
+    /**
+     * adapter that is responsible for displaying list of proverbs
+     */
+    private ProverbAdapter mAdapter;
+
+    /**
+     * a list view that accommodates the list of proverbs
+     */
+    private ListView mListView;
 
     public FragShowMulti() {
     }
@@ -28,12 +43,21 @@ public class FragShowMulti extends Fragment {
         return inflater.inflate(R.layout.frag_show_multi, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        this.mAdapter = new ProverbAdapter(getContext(), null);
+        this.mListView = (ListView) getActivity().findViewById(R.id.frag_show_multi_list);
+        this.mListView.setAdapter(this.mAdapter);
+    }
+
     public void load(List<Proverb> proverbs) {
         this.mProverbs = proverbs;
+        this.mAdapter.load(this.mProverbs);
+        Log.i(Config.APP_NAME, "loading proverbs " + proverbs.size());
     }
 
     public void updateView(){
-        /// TODO
-        Log.i(Config.APP_NAME, "updating view: " + this.mProverbs.size());
+        this.mAdapter.notifyDataSetChanged();
     }
 }
