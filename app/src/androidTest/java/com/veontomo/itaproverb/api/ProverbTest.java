c0bfdd68cb0f7efcc11584cad051e6d7e963e71c
Mario Rossi <veontomo@gmail.com>
2015-09-09 13:56:34 +0200

    test: add test for Proverb class

--- app/src/androidTest/java/com/veontomo/itaproverb/api/ProverbTest.java
+++ app/src/androidTest/java/com/veontomo/itaproverb/api/ProverbTest.java
@@ -1,47 +1,32 @@
 package com.veontomo.itaproverb.api;
 
-import junit.framework.TestCase;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Proverb-related unit tests
+ * Created by Mario Rossi on 09/09/2015 at 13:47.
  *
+ * @author veontomo@gmail.com
+ * @since xx.xx
  */
-public class ProverbTest extends TestCase {
+public class ProverbTest {
 
     @Before
     public void setUp() throws Exception {
 
     }
 
-    @Test
-    public void gettingTextOfANewProverb() {
-        Proverb proverb = new Proverb("When in Rome, do as the Romans.");
-        assertEquals("Proverb text must be When in Rome, do as the Romans.", "When in Rome, do as the Romans.", proverb.text);
+    @After
+    public void tearDown() throws Exception {
+
     }
 
     @Test
-    public void gettingTextOfExistingProverb() {
-        Proverb proverb = new Proverb(2, "Fortune favors the bold.");
-        assertEquals("Proverb text must be Fortune favors the bold.", "Fortune favors the bold.", proverb.text);
-    }
-
-    @Test
-    public void gettingIdOfANewProverb() {
+    public void idOfNonSavedProverbShoudBeMinusOne(){
         Proverb proverb = new Proverb("whatever");
-        assertEquals("Proverb id must be -1", -1, proverb.id);
+        assertEquals(-1, proverb.id);
     }
-
-    @Test
-    public void gettingIdOfExistingProverb() {
-        Proverb proverb = new Proverb(6, "whatever");
-        assertEquals("Proverb id must be 6", 6, proverb.id);
-    }
-
-
-
 }
