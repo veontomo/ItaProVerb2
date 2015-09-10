d7138daf5f89d1f19838d69217083021e778489d
Mario Rossi <veontomo@gmail.com>
2015-09-10 09:25:54 +0200

    feat: set up app logo

--- app/src/main/java/com/veontomo/itaproverb/activities/ActManager.java
+++ app/src/main/java/com/veontomo/itaproverb/activities/ActManager.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 import com.veontomo.itaproverb.R;
 
-public class Manager extends AppCompatActivity {
+public class ActManager extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
