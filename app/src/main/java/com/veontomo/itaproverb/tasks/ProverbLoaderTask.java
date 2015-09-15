befd4955526301198bbebda7c3cf562e2d06c704
Mario Rossi <veontomo@gmail.com>
2015-09-15 22:27:21 +0200

    feat: remove ActAll activity

--- app/src/main/java/com/veontomo/itaproverb/tasks/ProverbLoaderTask.java
+++ app/src/main/java/com/veontomo/itaproverb/tasks/ProverbLoaderTask.java
@@ -2,9 +2,11 @@ package com.veontomo.itaproverb.tasks;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.veontomo.itaproverb.R;
+import com.veontomo.itaproverb.api.Config;
 import com.veontomo.itaproverb.api.Storage;
 
 import java.io.BufferedReader;
