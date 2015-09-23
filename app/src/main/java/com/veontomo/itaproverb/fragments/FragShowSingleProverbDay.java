b7479b4dd0d2c4952a0da7d4d245140bfbdb41f6
Mario Rossi <veontomo@gmail.com>
2015-09-23 16:00:13 +0200

    feat: add a layout for proverb-day fragment

--- app/src/main/java/com/veontomo/itaproverb/fragments/FragShowSingleProverbDay.java
+++ app/src/main/java/com/veontomo/itaproverb/fragments/FragShowSingleProverbDay.java
@@ -1,5 +1,14 @@
 package com.veontomo.itaproverb.fragments;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.veontomo.itaproverb.R;
+import com.veontomo.itaproverb.api.Config;
+
 /**
  * Created by Mario Rossi on 23/09/2015 at 12:25.
  *
@@ -7,4 +16,11 @@ package com.veontomo.itaproverb.fragments;
  * @since xx.xx
  */
 public class FragShowSingleProverbDay extends FragShowSingle {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.i(Config.APP_NAME, "show single " + Thread.currentThread().getStackTrace()[2].getMethodName());
+        return inflater.inflate(R.layout.frag_show_single_proverb_day, container, false);
+    }
 }
