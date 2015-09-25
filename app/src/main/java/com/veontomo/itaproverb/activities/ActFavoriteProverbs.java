4b2d8549cf0b663fb338bbcc7f72449d5f88ece5
Mario Rossi <veontomo@gmail.com>
2015-09-25 22:02:02 +0200

    feat: add view in case no favorite proverbs are available

--- app/src/main/java/com/veontomo/itaproverb/activities/ActFavoriteProverbs.java
+++ app/src/main/java/com/veontomo/itaproverb/activities/ActFavoriteProverbs.java
@@ -1,5 +1,10 @@
 package com.veontomo.itaproverb.activities;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.veontomo.itaproverb.R;
 import com.veontomo.itaproverb.api.Proverb;
 import com.veontomo.itaproverb.api.Storage;
 
@@ -14,5 +19,16 @@ public class ActFavoriteProverbs extends ActMultiBase {
     @Override
     public List<Proverb> getItems(Storage storage) {
         return storage.getFavorites();
+    }
+
+    /**
+     * Disables search input field
+     */
+    @Override
+    public void onEmptyInput() {
+        super.onEmptyInput();
+        View v = getLayoutInflater().inflate(R.layout.empty_input, null);
+        ViewGroup insertPoint = (ViewGroup) findViewById(R.id.act_favorites_root);
+        insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 }
