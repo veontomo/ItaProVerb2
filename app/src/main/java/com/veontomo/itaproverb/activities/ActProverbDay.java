10c19243a419f180e1b00040f254770ea099c57b
Mario Rossi <veontomo@gmail.com>
2015-09-30 21:51:39 +0200

    fix: use single-base layout for proverb-of-day activity as well

--- app/src/main/java/com/veontomo/itaproverb/activities/ActProverbDay.java
+++ app/src/main/java/com/veontomo/itaproverb/activities/ActProverbDay.java
@@ -40,7 +40,7 @@ public class ActProverbDay extends ActSingleBase {
             Config.strictModeInit();
         }
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_proverb_day);
+        setContentView(R.layout.act_single_base);
     }
 
     @Override
