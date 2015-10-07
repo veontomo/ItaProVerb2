d34e3bf6658040bed2ab427abc967f256ccd879a
Mario Rossi <veontomo@gmail.com>
2015-10-07 16:07:44 +0200

    test: create test for edit-and-back scenario

--- app/src/androidTest/java/com/veontomo/itaproverb/activities/ActOracleTest.java
+++ app/src/androidTest/java/com/veontomo/itaproverb/activities/ActOracleTest.java
@@ -3,14 +3,13 @@ package com.veontomo.itaproverb.activities;
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
+import android.widget.TextView;
 
 import com.veontomo.itaproverb.R;
-
-import junit.framework.TestCase;
+import com.veontomo.itaproverb.api.Logger;
 
 /**
  * Test suit for random proverb
- *
  */
 public class ActOracleTest extends ActivityInstrumentationTestCase2<ActOracle> {
 
@@ -24,11 +23,7 @@ public class ActOracleTest extends ActivityInstrumentationTestCase2<ActOracle> {
     public void setUp() throws Exception {
         super.setUp();
         mActivity = getActivity();
-        mEdit =  mActivity.findViewById(R.id.frag_manager_edit);
-    }
-
-    public void tearDown() throws Exception {
-
+        mEdit = mActivity.findViewById(R.id.frag_manager_edit);
     }
 
     public void testPreconditions() {
@@ -38,8 +33,13 @@ public class ActOracleTest extends ActivityInstrumentationTestCase2<ActOracle> {
         assertNotNull("Edit button is not found", mEdit);
     }
 
+    public void testProverbTextFieldIsNotEmpty() {
+        final TextView tv = (TextView) mActivity.findViewById(R.id.frag_show_single_text);
+        final String text = tv.getText().toString();
+        assertNotNull("Random proverb must display a text", text);
+    }
 
-    public void testEdit(){
+    public void testEditButtonMustRedirectToEditPage() {
         Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ActEdit.class.getName(), null, false);
         mActivity.runOnUiThread(new Runnable() {
             @Override
@@ -51,5 +51,47 @@ public class ActOracleTest extends ActivityInstrumentationTestCase2<ActOracle> {
 
         assertNotNull("Click on all-proverb placeholder must start an activity", nextActivity);
         nextActivity.finish();
+    }
+
+    /**
+     * Scenario: clicking "edit" and then "back" buttons
+     *     When I am in "Fortune teller" screen
+     *     And I see a proverb text
+     *     And I click "edit" button
+     *     When "Edit Proverb" activity loads
+     *     And I click "back" button
+     *     When "Fortune teller" screen re-appears
+     *     Then I should see the original proverb text
+     */
+    public void testClickEditAndTurnBack() {
+        // getting initial content of the activity
+        final TextView tv = (TextView) mActivity.findViewById(R.id.frag_show_single_text);
+        final String originalText = tv.getText().toString();
+
+        // launching edit activity
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ActEdit.class.getName(), null, false);
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mEdit.performClick();
+            }
+        });
+        final ActEdit editActivity = (ActEdit) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 1000);
+
+        // pressing "back" button when in edit activity in order to return to the original activity
+        Instrumentation.ActivityMonitor activityMonitor2 = getInstrumentation().addMonitor(ActOracle.class.getName(), null, false);
+        editActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                editActivity.onBackPressed();
+            }
+        });
+        // original activity recreated
+        final ActOracle actRecreated = (ActOracle) getInstrumentation().waitForMonitorWithTimeout(activityMonitor2, 1000);
+        // getting content of the recreated activity
+        final TextView tv2 = (TextView) actRecreated.findViewById(R.id.frag_show_single_text);
+        final String resultingText = tv2.getText().toString();
+
+        assertEquals("Oracle activity should maintain the proverb text when clicking on edit and then coming back", originalText, resultingText);
     }
 }
